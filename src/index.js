c7f4d04b2fee0bebf909d6b711bb4eb6e46a84b3
Dave <dave@stackden.com>
2018-04-13 18:06:11 -0700

    Movie quote get completed

--- src/index.js
+++ src/index.js
@@ -4,12 +4,19 @@ import {Provider} from "react-redux";
 import {createStore, applyMiddleware} from "redux";
 import rootReducer from './reducers';
 import {BrowserRouter as Router} from 'react-router-dom';
+import thunk from 'redux-thunk';
+import types from "./actions/types"
 
 
 import App from './components/app';
 
-const store = createStore(rootReducer, {}, applyMiddleware());
+const store = createStore(rootReducer, {}, applyMiddleware(thunk));
 
+if(localStorage.getItem('token')){
+    store.dispatch({
+        type:types.SIGN_IN
+    })
+}
 
 ReactDOM.render(
     <Provider store={store}>
